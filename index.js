4ef5a112d5b316a072da27604ecda7e9639236a1
Linda Pialova <lindapialova1@gmail.com>
2023-11-25 13:27:28 +0100

    priklad dovednosti

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log('funguju!');
+
+function updateSkill(skillId, value) {
+    const skillBar = document.getElementById(skillId).querySelector('.skill__progress')
+    const skillValue = document.getElementById(skillId).querySelector('.skill__value')
+  
+    skillBar.style.width = value + "%"
+    skillValue.textContent = value + " / 100 "
+  }
+  
+  
+  const HodnotaHTML = prompt("Zadejte úroveň HTML (0-100): ")
+  const HodnotaCSS = prompt("Zadejte úroveň CSS (0-100): ")
+  const HodnotaJS = prompt("Zadejte úroveň JavaScript (0-100): ")
+ 
+  updateSkill('skill1', Number(HodnotaHTML, 10) || 0)
+  updateSkill('skill2', Number(HodnotaCSS, 10) || 0)
+  updateSkill('skill3', Number(HodnotaJS, 10) || 0)
